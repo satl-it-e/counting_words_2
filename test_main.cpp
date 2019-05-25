0ab198a081f6391ddaaa1600eac3282d7f6c7b13
LAnastasiia <lazorenko@ucu.edu.ua>
2019-05-25 13:04:43 +0300

    fix queue initializaion inside constructor (new)!

--- test_main.cpp
+++ test_main.cpp
@@ -11,27 +11,33 @@
 
 template<typename T>
 class MyQueue{
+
     private:
         std::mutex mtx;
         std::condition_variable cv;
-        std::queue<T> *q;
+        std::queue<T> *q = new std::queue<T>;
+        bool notified = false;                         // https://ru.cppreference.com/w/cpp/thread/condition_variable
+
     public:
         bool empty(){ return q->empty(); }
 
-        void push(T &element){
+        void push(T element){
             std::unique_lock<std::mutex> lock(mtx);
-            q->emplace(element);
+            q->push(element);
+            notified = true;
             cv.notify_one();
         }
 
         void pop(){
             std::unique_lock<std::mutex> lock(mtx);
+            cv.wait(lock, [this]() { return notified;});
             while(!q->empty()){
                 T *element = reinterpret_cast<T*>(malloc(sizeof(T)));
                 *element = q->back();
                 q->pop();
                 std::cout << element;
             }
+            notified = false;
         }
 
 };
@@ -39,7 +45,8 @@ class MyQueue{
 
 int main()
 {
-
+    MyQueue<std::string> mq;
+    mq.push("qwerty");
 
     return 0;
 }
