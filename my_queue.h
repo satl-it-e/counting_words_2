3a00722a1219b19af100a8802c65f3136521cdee
sofiia-tesliuk <sofiia.tesliuk@gmail.com>
2019-05-27 18:46:43 +0300

    moved queue to another file;

--- my_queue.h
+++ my_queue.h
@@ -1,5 +1,12 @@
+#ifndef COUNTING_WORDS_MY_QUEUE_H
+#define COUNTING_WORDS_MY_QUEUE_H
 
-template<typename T >
+#include <iostream>
+#include <mutex>
+#include <queue>
+
+
+template<typename T>
 class MyQueue{
 
 private:
@@ -10,9 +17,48 @@ private:
     bool notified = false;
 
 public:
-    void set_producers_number(int num);
-    void finish();
-    void push(T element);
-    bool can_try_pop(int n);
-    std::vector<T> pop(int n);
+    void set_producers_number(int num){
+        producers_num = num;
+    }
+    void finish(){
+        std::lock_guard<std::mutex> lock(mtx);
+        producers_num--;
+        if (producers_num == 0){
+        }
+        notified = true;
+        cv.notify_all();
+    }
+
+    void push(T element){
+        std::unique_lock<std::mutex> lock(mtx);
+        q->push(element);
+        notified = true;
+        cv.notify_all();
+    }
+
+    bool can_try_pop(int n){
+        return (producers_num > 0) || (q->size() >= n);
+    }
+
+    std::vector<T> pop(int n){
+        std::vector<T> some;
+        std::unique_lock<std::mutex> lock(mtx);
+        while ((producers_num > 0) || (q->size() >= n)) {
+            while(!notified && (q->size() < n)){
+                cv.wait(lock);
+            }
+            while(q->size() >= n) {
+                while (n > 0) {
+                    some.emplace_back(q->front());
+                    q->pop();
+                    n--;
+                }
+                return some;
+            }
+            notified = false;
+        }
+        return some;
+    }
 };
+
+#endif //COUNTING_WORDS_MY_QUEUE_H
