4fbfcfc16c8d67332950cee22d4536a72292fc6d
LAnastasiia <lazorenko@ucu.edu.ua>
2019-05-27 18:02:34 +0300

    separated my_queue

--- my_queue.cpp
+++ my_queue.cpp
@@ -1,37 +1,39 @@
 #include "my_queue.h"
 
-template<typename T >
-void MyQueue<T>::set_producers_number(int num){
+
+void MyQueue::set_producers_number(int num){
     producers_num = num;
 }
 
 
-template<typename T >
-void MyQueue<T>::finish(){
-        std::lock_guard<std::mutex> lock(mtx);
-        producers_num--;
-        cv.notify_all();
+void MyQueue::finish(){
+    std::lock_guard<std::mutex> lock(mtx);
+    producers_num--;
+    if (producers_num == 0){
     }
+    notified = true;
+    cv.notify_all();
+}
 
-template<typename T >
-void MyQueue<T>::push(T element){
-        std::unique_lock<std::mutex> lock(mtx);
-        q->push(element);
-        notified = true;
-        cv.notify_all();
-    }
 
-template<typename T >
-bool MyQueue<T>::can_try_pop(int n){
+void MyQueue::push(T element){
+    std::unique_lock<std::mutex> lock(mtx);
+    q->push(element);
+    notified = true;
+    cv.notify_all();
+}
+
+
+bool MyQueue::can_try_pop(int n){
     return (producers_num > 0) || (q->size() >= n);
 }
 
-template<typename T >
-std::vector<T> MyQueue<T>::pop(int n){
+
+std::vector<T> MyQueue::pop(int n){
     std::vector<T> some;
     std::unique_lock<std::mutex> lock(mtx);
     while ((producers_num > 0) || (q->size() >= n)) {
-        while(!notified){
+        while(!notified && (q->size() < n)){
             cv.wait(lock);
         }
         while(q->size() >= n) {
@@ -46,4 +48,3 @@ std::vector<T> MyQueue<T>::pop(int n){
     }
     return some;
 }
-
--- test_main.cpp
+++ test_main.cpp
@@ -24,65 +24,9 @@
 #include "config.h"
 #include "additional.h"
 #include "my_archive.h"
-//#include "my_queue.h"
-//#include "index.h"
-//#include "merge.h"
+#include "my_queue.h"
 
-template<typename T >
-class MyQueue{
 
-private:
-    std::mutex mtx;
-    std::condition_variable cv; // https://ru.cppreference.com/w/cpp/thread/condition_variable
-    std::queue<T> *q = new std::queue<T>;
-    int producers_num = 0;
-    bool notified = false;
-
-public:
-    void set_producers_number(int num){
-        producers_num = num;
-    }
-
-    void finish(){
-        std::lock_guard<std::mutex> lock(mtx);
-        producers_num--;
-        if (producers_num == 0){
-        }
-        notified = true;
-        cv.notify_all();
-    }
-
-    void push(T element){
-        std::unique_lock<std::mutex> lock(mtx);
-        q->push(element);
-        notified = true;
-        cv.notify_all();
-    }
-
-    bool can_try_pop(int n){
-        return (producers_num > 0) || (q->size() >= n);
-    }
-
-    std::vector<T> pop(int n){
-        std::vector<T> some;
-        std::unique_lock<std::mutex> lock(mtx);
-        while ((producers_num > 0) || (q->size() >= n)) {
-            while(!notified && (q->size() < n)){
-                cv.wait(lock);
-            }
-            while(q->size() >= n) {
-                while (n > 0) {
-                    some.emplace_back(q->front());
-                    q->pop();
-                    n--;
-                }
-                return some;
-            }
-            notified = false;
-        }
-        return some;
-    }
-};
 
 using namespace boost::locale::boundary;
 
